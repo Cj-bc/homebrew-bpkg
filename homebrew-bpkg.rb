49ed5b8aa83426de2dd06aceba99ad8fffca474c
Cj-bc <cj-bc@y-modify.org>
2018-04-21 23:39:22 +0900

    Add: installing section completely.
    test passed

--- homebrew-bpkg.rb
+++ homebrew-bpkg.rb
@@ -3,14 +3,18 @@ class Bpkg < Formula
   url "https://gihub.com/bpkg/bpkg/archive/0.3.0.tar.gz"
   sha256 "ee86749e0d322fba426e06fabfc53d50172f064240c19cdbdfa4a02475a6a247"
 
-  def install do
+  def install
 
-    cmd = [ "json", "install", "package", "term", "suggest", "init", "utils", "update", "list", "show", "getdeps" ]
+    cmd = ["json", "install", "package", "term", "suggest", "init", "utils", "update", "list", "show", "getdeps"]
+    ohai "renaming bpkg.sh to bpkg..."
+    system "mv", "bpkg.sh", "bpkg"
     bin.install "bpkg"
-    for CMD in cmd do
-      bin.install "bpkg-#{CMD}"
+    for command in cmd do
+      ohai "renaming #{command}.sh..."
+      system "mv", "./lib/#{command}/#{command}.sh", "bpkg-#{command}"
+      bin.install "bpkg-#{command}"
     end
-
+    ohai "All programs are installed successfully."
   end
 
   test do
